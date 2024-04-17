63cdb60f66bf2a18eedc1f3aded10d3ea45561b3
AriSongYe <kow14671@gmail.com>
2024-04-17 22:49:40 +0900

    Add Shimmer Effect

--- src/components/ui/Skeleton/preBoxSkeleton.js
+++ src/components/ui/Skeleton/preBoxSkeleton.js
@@ -1,10 +1,15 @@
 import './PreBoxSkeleton.css'; // 스켈레톤 애니메이션을 위한 CSS 파일
+import Shimmer from './Shimmer';
 
 function PreBoxSkeleton() {
 return (
     <div className="pre-box-skeleton">
-        <div className="skeleton-img"></div>
-        <div className="skeleton-title"></div>
+        <div className="skeleton-img">
+            <Shimmer/>
+        </div>
+        <div className="skeleton-title">
+            <Shimmer/>
+        </div>
     </div>
 );
 }
