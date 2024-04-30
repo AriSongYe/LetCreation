74e1b4d42c7a2d36030c9a81fd012da8a1bfe918
AriSongYe <kow14671@gmail.com>
2024-04-30 17:55:57 +0900

    Change image alt

--- src/components/main/Box/preBox.js
+++ src/components/main/Box/preBox.js
@@ -18,12 +18,12 @@ function PreBox({ data }) {
     return (
         <div className="pre-box" >
             <div className="pre-box-container" onClick={openModal}>
-                <img className="pre-img" src={`http://localhost:3000/api/imgs/${data.imagePath}`} alt="이미지"></img>
+                <img className="pre-img" src={`http://localhost:3000/api/imgs/${data.imagePath}`} alt="Preview 이미지"></img>
                 <div className="pre-title">{data.title}</div>
             </div>
         <Modal isOpen={isModalOpen} onRequestClose={closeModal} contentLabel="Program Modal" shouldCloseOnOverlayClick={true}>
             <h1>{data.title}</h1>
-            <img className="modal-image" src={`http://localhost:3000/api/imgs/${data.imagePath}`} alt="Image" />
+            <img className="modal-image" src={`http://localhost:3000/api/imgs/${data.imagePath}`} alt="Modal inner 이미지" />
             <p>{data.summary}</p>
             <DownButton filepath={data.filePath}/>
             <div className="comment-container">
